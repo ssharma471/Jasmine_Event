93972a7bf7e2652d725e7bdb4a33165faa85de94
ssharma471 <sidhantsharmavlog@gmail.com>
2025-04-04 01:58:37 -0400

    Added more

--- assets/js/main.js
+++ assets/js/main.js
@@ -155,4 +155,6 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
   });
   
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+
   
